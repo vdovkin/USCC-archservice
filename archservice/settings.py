7e9935169b3ffccd10a481af5d8094a8cdcb3d2a
Vdovkin Oleksandr <a.a.vdovkin@gmail.com>
2020-12-12 01:04:38 +0200

    add contacts app and contacts template

--- archservice/settings.py
+++ archservice/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
+    'contacts.apps.ContactsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
